8933a5bac489c6348cca7f9524b52e36a844484f
kimsaram32 <kimsaram32@proton.me>
2025-05-04 19:45:43 +0900

    Fix library tab bar title

--- spotify-clone/Controllers/Core/TabBarViewController.swift
+++ spotify-clone/Controllers/Core/TabBarViewController.swift
@@ -5,7 +5,7 @@ class TabBarViewController: UITabBarController {
     private let tabItems: [(UIViewController, UITabBarItem)] = [
         (HomeViewController(), UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), tag: 0)),
         (SearchViewController(), UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)),
-        (LibraryViewController(), UITabBarItem(title: "Home", image: UIImage(systemName: "music.note.list"), tag: 2)),
+        (LibraryViewController(), UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 2)),
     ]
 
     override func viewDidLoad() {
